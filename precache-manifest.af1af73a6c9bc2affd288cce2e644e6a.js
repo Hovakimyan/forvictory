0b50633911c0695b540875b1f8f70bc092a47cab
Hovakimyan <Hovakimyan@users.noreply.github.com>
2020-10-03 23:30:43 +0000

    deploy: ef12d9e871d497b53dcf087658c52cd6aecacd08

---
+++ precache-manifest.af1af73a6c9bc2affd288cce2e644e6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7339e0b549bbfb9a6d5c63b6ec11faed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b16948672f6585df3e14",
+    "url": "/static/js/2.3925deb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3925deb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8c16975e971c21cc5e",
+    "url": "/static/js/main.82bcb6d6.chunk.js"
+  },
+  {
+    "revision": "136cd1d39de23ee847ba",
+    "url": "/static/js/runtime-main.5c6722b2.js"
+  },
+  {
+    "revision": "e21327386c16b1635d47a8a5deff0dec",
+    "url": "/static/media/flag-en.e2132738.png"
+  },
+  {
+    "revision": "dd219dc5783e8c5bfe7542d4cbbbd18b",
+    "url": "/static/media/jet.dd219dc5.png"
+  },
+  {
+    "revision": "fc9dcf671e09296af58544e3a4363e10",
+    "url": "/static/media/solider.fc9dcf67.png"
+  },
+  {
+    "revision": "d730104443cc58f841a971f8b4cf837d",
+    "url": "/static/media/tank.d7301044.png"
+  }
+]);
