1744214382a5e8b2c7ab0329097914e68eb6674f
Hovakimyan <Hovakimyan@users.noreply.github.com>
2020-10-03 23:39:44 +0000

    deploy: 846bf88848b55322dd28be91726e2183a3d50383

---
+++ precache-manifest.11718e623f13e9b852e512334492f80a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c69deecaa13e2da42cd1a882f7fe284",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93eef03b69923e37503d",
+    "url": "./static/js/2.ee453171.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ee453171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8c16975e971c21cc5e",
+    "url": "./static/js/main.82bcb6d6.chunk.js"
+  },
+  {
+    "revision": "ec1d60d49a10813c6ec4",
+    "url": "./static/js/runtime-main.dc4c5c86.js"
+  },
+  {
+    "revision": "e21327386c16b1635d47a8a5deff0dec",
+    "url": "./static/media/flag-en.e2132738.png"
+  },
+  {
+    "revision": "dd219dc5783e8c5bfe7542d4cbbbd18b",
+    "url": "./static/media/jet.dd219dc5.png"
+  },
+  {
+    "revision": "fc9dcf671e09296af58544e3a4363e10",
+    "url": "./static/media/solider.fc9dcf67.png"
+  },
+  {
+    "revision": "d730104443cc58f841a971f8b4cf837d",
+    "url": "./static/media/tank.d7301044.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af1af73a6c9bc2affd288cce2e644e6a.js"
+  "./precache-manifest.11718e623f13e9b852e512334492f80a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
