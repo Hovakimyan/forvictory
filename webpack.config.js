f5e3d557675ad7923703ae22a69325d66315f129
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-10-03 21:22:35 +0400

    fix: update configs

--- webpack.config.js
+++ webpack.config.js
@@ -53,12 +53,12 @@ module.exports = {
     ],
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         publicPath: '/',
         filename: `bundle.js`,
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './build',
         disableHostCheck: true,
         historyApiFallback: true,
         port: 1234,
