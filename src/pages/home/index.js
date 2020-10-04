cb7be78680c88358c10ac052ed2ddc3cfdf892d8
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-10-04 22:02:15 +0400

    add descriptions

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,7 @@ import * as Styled from './styled'
 import List from './list'
 import { useTranslation } from 'react-i18next'
 import i18n, { languages } from '../../i18n';
+import Helmet from "react-helmet";
 
 const Home = (props) => {
     const { lang }= props.match.params
@@ -19,6 +20,9 @@ const Home = (props) => {
     const { t } = useTranslation();
     return (
         <Styled.Container>
+            <Helmet>
+                <title>{t("TITLE")}</title>
+            </Helmet>
             <Styled.Title>{t("TITLE")}</Styled.Title>
             <List />
             <Styled.Description>
